f21329355282ce8001edc18202309d891f084a97
Rajsinh123 <rajsinhmandawale15@gmail.com>
2024-06-01 10:43:46 +0530

    Initial Commit

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+
+    class Program
+    {
+
+        private string name;
+        public string Name { get; set; }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Name = "rajsinh";
+            Console.WriteLine(p.Name);
+            Console.ReadLine();
+        }
+    }
+}
